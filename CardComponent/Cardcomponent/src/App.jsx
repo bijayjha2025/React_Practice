62d74b4f472ec782de0b9a38a76017e38b09ec2e
Bijay Jha <bijayjha12345@gmail.com>
2025-11-25 20:44:08 +0545

    Array of Objects

--- CardComponent/Cardcomponent/src/App.jsx
+++ CardComponent/Cardcomponent/src/App.jsx
@@ -1,8 +1,40 @@
 
-import Card from "./Card";
+import Card from "./Card.jsx";
 
 function App(){
-  return(
+
+  const teamMembers = [
+    {
+      id:1,
+      name: "Elon Mosque",
+      position: "CEO and Founder",
+      decription: "With over 20 years of experience, Mr. Mosque is leading this team beautifully. His commitments and ideas are praiseworthy and are always for our betterment.",
+      imageUrl: ""
+    },
+    {
+      id:2,
+      name: "Tim Pen",
+      position: "Operations Head",
+      decription: "With 10 years of experience and expertise in operations, Mr. Pen is one of our best performing employees. His attention to detail and communication skills are always on point.",
+      imageUrl: ""
+    },
+    {
+      id:3,
+      name: "Steve Job",
+      position: "CEO and Founder",
+      decription: "With over 20 years of experience, Mr. Mosque is leading this team beautifully. His commitments and ideas are praiseworthy and are always for our betterment.",
+      imageUrl: ""
+    },
+    {
+      id:1,
+      name: "Elon Mosque",
+      position: "CEO and Founder",
+      decription: "With over 20 years of experience, Mr. Mosque is leading this team beautifully. His commitments and ideas are praiseworthy and are always for our betterment.",
+      imageUrl: ""
+    }
+    ];
+     
+    return(
     <>
     
     </>
