a5f29f4a6841ad73fd9cbd6ade878f8a2231c9bc
Bijay Jha <bijayjha12345@gmail.com>
2025-11-25 20:33:55 +0545

    Card component setup

--- CardComponent/Cardcomponent/src/Card.jsx
+++ CardComponent/Cardcomponent/src/Card.jsx
@@ -1,8 +1,17 @@
 
-function Card(){
+function Card({name, position, description, imageUrl}){
     return(
         <>
-        
+        <div className="cardContainer">
+            <div className="cardImage">
+                <img src="" alt="" />
+            </div>
+            <div className="cardContent">
+                <h3 className="name">{Name}</h3>
+                <p className="position">{Position}</p>
+                <p className="description">{Description}</p>
+            </div>
+        </div>
         </>
     );
 }
