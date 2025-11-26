07573172c5a68f33777cd704d7292c783652c8f2
Bijay Jha <bijayjha12345@gmail.com>
2025-11-26 16:59:41 +0545

    Images inclusion

--- CardComponent/Cardcomponent/src/App.jsx
+++ CardComponent/Cardcomponent/src/App.jsx
@@ -9,28 +9,28 @@ function App(){
       name: "Dragon",
       nickname: "Sky Inferno",
       description: "The Dragon is a flying beast of pure destruction, unleashing scorching fire that melts defenses in seconds while its massive health and overwhelming power dominate the skies and turn enemy bases into ashes.",
-      imageUrl: ""
+      imageUrl: "/Images/dragon.jpg",
     },
     {
       id:2,
       name: "Archer",
       nickname: "Shadow Arrow",
       description: "Swift and precise, the Archer rains down deadly arrows from a safe distance, using her unmatched accuracy and agility to chip away at defenses long before they can threaten her presence on the battlefield.",
-      imageUrl: ""
+      imageUrl: "/Images/archer.jpg"
     },
     {
       id:3,
       name: "Wizard",
       nickname: "Flame Sage",
       description: "Harnessing volatile magical energy with effortless mastery, the Wizard hurls blazing fireballs that explode on impact, obliterating groups of enemies and defenses in moments while lighting the battlefield with sheer arcane destruction.",
-      imageUrl: ""
+      imageUrl: "/Images/wizard.jpg"
     },
     {
       id:4,
       name: "Pekka",
       nickname: "Iron Reaper",
       description: "A towering machine of unmatched strength, P.E.K.K.A marches forward with unstoppable force, slicing through walls, troops, and defenses alike as her brutal power turns even the toughest enemy structures into shattered debris.",
-      imageUrl: ""
+      imageUrl: "/Images/pekka.jpg"
     }
     ];
      
