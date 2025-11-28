2a58e1d9bf5229db1fb3b51d0d20aa3e03f7a765
Bijay Jha <bijayjha12345@gmail.com>
2025-11-28 12:51:26 +0545

    Handlers for adding, deleting, read, favorite books

--- PersonalBookManager/bookmanager/src/App.jsx
+++ PersonalBookManager/bookmanager/src/App.jsx
@@ -1,16 +1,43 @@
 
 import React, {useState} from 'react'
-import Addbook from "./AddBook.jsx"
-import Booklist from "./BookList.jsx"
+import Addbook from "./Components/AddBook.jsx"
+import Booklist from "./Components/BookList.jsx"
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState([]);
 
-  return (
+  const handleAddBook = (book) => {
+    setBooks([book, ...books]);
+  };
+
+  const handleToggleRead = (id) =>{
+    setBooks(
+      books.map((b) =>
+      b.id === id ? {...b, isRead: !b.isRead }: b
+    )
+    );
+  }
+
+  const handleDeleteBook = (id) => {
+    setBooks(books.filter((b) => b.id !== id));
+  }
+
+    const handleToggleFavorite = (id) => {
+    setBooks(
+      books.map((b) =>
+        b.id === id ? { ...b, isFavorite: !b.isFavorite } : b
+      )
+    );
+  };
+
+
+   return (
     <>
      <Addbook></Addbook>
      <Booklist></Booklist>
     </>
   )
-}
+
+  };
 
 export default App
