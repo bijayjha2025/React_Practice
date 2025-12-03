eba4b5579fbb2df264db5a497eafae4d565706d1
Bijay Jha <bijayjha12345@gmail.com>
2025-12-03 16:03:52 +0545

    Basic Setup

---
+++ RandomJokeGenerator/ApiUnderstanding/src/App.jsx
@@ -0,0 +1,13 @@
+
+import JokeGenerator from "./JokeGenerator.jsx"
+
+function App() {
+
+  return (
+    <div>
+    <JokeGenerator></JokeGenerator>
+    </div>
+  );
+}
+
+export default App
---
+++ RandomJokeGenerator/ApiUnderstanding/src/JokeGenerator.jsx
@@ -0,0 +1,35 @@
+
+import { useState, useEffect} from 'react'
+
+function JokeGenerator(){
+    const [joke, setJoke] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const LoadJoke = async()=>{
+        setLoading(true)
+    }
+
+
+    useEffect (()=>{
+        LoadJoke();
+    }, []);
+
+    return(
+        <div>
+            <h2>Practing API Fetching.</h2>
+            <h3>Random Joke Loader</h3>
+
+            { loading ? (
+                <p>Loading joke....</p>): (
+                    <div>
+                        <h3>{joke.setup}</h3>
+                        <p><strong>{joke.punchline}</strong></p>
+                    </div>
+                )}
+
+            <button onClick={LoadJoke}>Load Joke</button>
+        </div>
+    );
+}
+
+export default JokeGenerator
