2ea089559c872c7eddd7dcce94e2084c559a4e27
Bijay Jha <bijayjha12345@gmail.com>
2025-12-08 22:39:09 +0545

    Home section

--- GadgetCatalog/gadgetcatalog/src/Pages/Home.jsx
+++ GadgetCatalog/gadgetcatalog/src/Pages/Home.jsx
@@ -1,9 +1,24 @@
 
+import React from "react";
+import { gadgets } from "../Data/Gadgets";
+import { Link } from "react-router-dom";
+
+
 function Home(){
     return(
-        <>
-        <h1>This is home page.</h1>
-        </>
+        <div>
+            {
+                gadgets.map((g)=> (
+                    <div key={g.id}>
+                        <img src={g.name} alt={g.name}/>
+                        <h2>{g.name}</h2>
+                        <p>{g.brand}</p>
+                        <p><strong>{g.price}</strong></p>
+
+                        <Link to={`/gadgets/${g.id}`}>View Details</Link>
+                    </div>
+                ))}
+        </div>
     );
 }
 
