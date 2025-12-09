0250ab2852aee88672b1830bfad492d00a970d3f
Bijay Jha <bijayjha12345@gmail.com>
2025-12-09 19:04:37 +0545

    hero Image section

--- GadgetCatalog/gadgetcatalog/src/Components/HeroSection.jsx
+++ GadgetCatalog/gadgetcatalog/src/Components/HeroSection.jsx
@@ -1,17 +1,20 @@
 
 import { useNavigate } from "react-router-dom";
+import HeroImage from '../Images/HeroSection.jpg';
 
 function HeroSection() {
   const navigate = useNavigate();
 
   return (
     <div className="heroSectionContainer">
-      <h1>Discover the Latest Tech Gadgets</h1>
-      <p>Explore a curated collection of modern devices—smartphones, speakers, wearables, laptops, and more.</p>
+      <img src= {HeroImage} alt="HeroSectionImage" className="heroSectionImage"></img>
 
-      <button onClick={() => navigate("/gadgets")}>
-        Start Browsing
-      </button>
+      <div className="heroContent">
+       <h1>Discover the Latest Tech Gadgets</h1>
+       <p>Explore a curated collection of modern devices—smartphones, speakers, wearables, laptops, and more.</p>
+
+       <button onClick={() => navigate("/gadgets")}>Start Browsing</button>
+      </div>
     </div>
   );
 }
