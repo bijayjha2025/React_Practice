4a53a84af5184ee6a3c17efb9a87afa767e54833
Bijay Jha <bijayjha12345@gmail.com>
2025-12-12 18:55:29 +0545

    Userprofile styling

--- UserAuthentication/userauthentication/src/Components/UserProfile.jsx
+++ UserAuthentication/userauthentication/src/Components/UserProfile.jsx
@@ -4,15 +4,37 @@ import { UseAuth } from "../Hooks/UseAuth";
 function UserProfile(){
     const { user, isLoggedIn } = UseAuth();
     
-    if (!isLoggedIn) return <p>Please log in to see profile.</p>;
-    
+    if (!isLoggedIn){
     return (
-    <div>
-      <h2>User Profile</h2>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
+    <div className="profileContainer">
+      <div className="containerIcon">👤</div>
+      <p className="containerText">Login to see profile</p>
     </div>
     );
+   }
+
+   return(
+    <div className="userProfile">
+      <div className="profileHeader">
+        <div className="profileImage">{user.name.charAt(0).toUpperCase()}</div>
+        <h2 className="profileHeading">User Profile</h2>
+        </div>
+        <div className="profileDetails">
+          <div className="profileItem">
+            <span className="profileLabel">Name:</span>
+            <span className="profileValue">{user.name}</span>
+          </div>
+          <div className="profileItem">
+            <span className="profileLabel">Email:</span>
+            <span className="profileValue">{user.email}</span>
+          </div>
+        </div>
+
+    </div>
+   );
+
 }
+
+
 
 export default UserProfile
