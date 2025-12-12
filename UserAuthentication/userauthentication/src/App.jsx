5af26defd6133f31910003529bd4e23dfc81d431
Bijay Jha <bijayjha12345@gmail.com>
2025-12-12 12:58:49 +0545

    Basic CSS for App

--- UserAuthentication/userauthentication/src/App.jsx
+++ UserAuthentication/userauthentication/src/App.jsx
@@ -3,15 +3,16 @@ import NavBar from "./Components/NavBar.jsx";
 import Home from "./Pages/Home.jsx";
 import Dashboard from "./Pages/Dashboard.jsx";
 import { UseAuth } from "./Hooks/UseAuth.jsx";
+import './App.css'
 
 function App() {
   const { isLoggedIn } = UseAuth();
   return (
-    <>
+    <div className = "appContainer">
       <NavBar />
       <Home />
       {isLoggedIn ? <Dashboard /> : <p>Please login to access dashboard</p>}
-    </>
+    </div>
   );
 }
 
