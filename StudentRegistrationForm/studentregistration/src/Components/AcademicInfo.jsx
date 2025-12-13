4905e2c036ad91c84078bc7cf18270c019eaec2f
Bijay Jha <bijayjha12345@gmail.com>
2025-12-13 23:00:50 +0545

    adding academic info

--- StudentRegistrationForm/studentregistration/src/Components/AcademicInfo.jsx
+++ StudentRegistrationForm/studentregistration/src/Components/AcademicInfo.jsx
@@ -1,10 +1,30 @@
 
+import React from "react";
 
-function AcademicInfo(){
+const AcademicInfo = ({formData, handleChange, nextStep, previousStep}) => {
     return(
-        <>
-        
-        </>
+        <div>
+            <h2>Step 2: Academic Information</h2>
+
+            <label>Student ID:</label>
+            <input type="text" name="studentId" value={formData.studentId} onChange={handleChange} placeholder="Enter student ID"/>
+
+            <label>Department:</label>
+            <select name="department" value={formData.department} onChange={handleChange}>
+            <option value="">Select Department</option>
+            <option value="Civil">Civil Engineering</option>
+            <option value="Computer">Computer Engineering</option>
+            <option value="Industrial">Industrial Engineering</option>
+            </select>
+
+
+            <input type="text" name="studentId" value={formData.studentId} onChange={handleChange} placeholder="Enter student ID"/>
+
+            <label>Student ID:</label>
+            <input type="text" name="studentId" value={formData.studentId} onChange={handleChange} placeholder="Enter student ID"/>
+
+
+        </div>
     );
 }
 
