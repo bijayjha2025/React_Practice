db0bdefd00d37968916d875dd81d602f3e4ce22b
Bijay Jha <bijayjha12345@gmail.com>
2025-12-14 16:23:22 +0545

    Submission

--- StudentRegistrationForm/studentregistration/src/Components/RegistrationForm.jsx
+++ StudentRegistrationForm/studentregistration/src/Components/RegistrationForm.jsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 import PersonalInfo from './PersonalInfo.jsx';
 import AcademicInfo from './AcademicInfo.jsx';
 import AccountSetup from './AccountSetup.jsx';
+import ReviewSubmit from './ReviewSubmit.jsx'
 
 const RegistrationForm = () => {
 
@@ -33,11 +34,33 @@ const RegistrationForm = () => {
         const prevStep = () =>{
             setStep(prevStep => prevStep -1);
         }
+
+    const handleSubmit = () => {
+        console.log("Completed", formData);
+        alert("Successful");
+
+        setFormData({
+            name: "",
+            email: "",
+            phone: "",
+            dateofBirth: "",
+            studentId: "",
+            department: "",
+            year: "",
+            username: "",
+            password: "",
+            confirmPassword: ""
+        });
+        setStep(1);
+    };
+
+
     return(
         <div>
             { step === 1 && ( <PersonalInfo formData={formData} handleChange={handleChange} nextStep={nextStep}/>)}
             { step === 2 && ( <AcademicInfo formData={formData} handleChange={handleChange} nextStep={nextStep} prevStep={prevStep}/>)}
             { step === 3 && ( <AccountSetup formData={formData} handleChange={handleChange} nextStep={nextStep} prevStep={prevStep}/>)}
+            { step === 4 && ( <ReviewSubmit formData={formData} prevStep={() => setStep(prevStep => prevStep -1 )} handleSubmit = {handleSubmit}/>)}
         </div>
     );
 }
---
+++ StudentRegistrationForm/studentregistration/src/Components/ReviewSubmit.jsx
@@ -0,0 +1,34 @@
+
+import React from "react";
+
+const ReviewSubmit = ({ formData, prevStep, handleSubmit }) => {
+    return(
+        <div>
+            <h2>Review and Submit</h2>
+
+            <h3>Personal Information</h3>
+            <p><strong>Full Name:</strong>{formData.name}</p>
+            <p><strong>Email:</strong>{formData.email}</p>
+            <p><strong>Phone:</strong>{formData.phone}</p>
+            <p><strong>Date of Birth:</strong>{formData.dateofBirth}</p>
+
+            <h3>Academic Information</h3>
+            <p><strong>Student ID:</strong>{formData.studentId}</p>
+            <p><strong>Department:</strong>{formData.department}</p>
+            <p><strong>Year:</strong>{formData.year}</p>
+
+            <h3>Account Information</h3>
+            <p><strong>Username:</strong>{formData.username}</p>
+            <p><strong>Password:</strong> ******** (hidden)</p>
+
+            <div>
+                <button onClick={prevStep}>Back</button>
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+
+        </div>
+    );
+
+};
+
+export default ReviewSubmit
